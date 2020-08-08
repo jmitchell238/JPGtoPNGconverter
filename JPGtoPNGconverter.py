6e192128c4165f64785bff0aac9601210020a6bd
James Mitchell <jmitchell238@gmail.com>
2020-08-07 23:44:05 -0500

    Adding starting project

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from PIL import Image
+
+# Grab the first and second argument
+print('Number of arguments: ', len(sys.argv))
+print('Argument 0 is: ', str(sys.argv[0]))
+print('Argument 1 is: ', sys.argv[1])
+print('Argument 2 is: ', sys.argv[2])
+
+# Check if new\ exists, if not create it
+
+
+
+# Loop through Pokedex, convert images to PNG
+# Save to the new folder.
+
+
