57b6bce97796fdecb9e5acfdad0c2a9e89763b71
James Mitchell <jmitchell238@gmail.com>
2020-08-09 18:55:15 -0500

    Updated JPGtoPNGconverter.py

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -2,19 +2,26 @@ import sys
 import os
 from PIL import Image
 
-# Working on learning Github
-
 # Grab the first and second argument
-print('Number of arguments: ', len(sys.argv))
-print('Argument 0 is: ', str(sys.argv[0]))
-print('Argument 1 is: ', sys.argv[1])
-print('Argument 2 is: ', sys.argv[2])
+img_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# print('Number of arguments: ', len(sys.argv))
+# print('Argument 0 is: ', str(sys.argv[0]))
+# print('Argument 1 is: ', sys.argv[1])
+# print('Argument 2 is: ', sys.argv[2])
 
 # Check if new\ exists, if not create it
-
-
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 # Loop through Pokedex, convert images to PNG
 # Save to the new folder.
+for file in os.listdir(img_folder):
+    img = Image.open(f'{img_folder}{file}')
+    clean_name = os.path.splitext(file)[0]
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('Image Converted')
+
 
 
